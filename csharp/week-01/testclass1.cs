bf316fa8285b4885dd26f5e792368eef573d2664
SharkinsPrime <sharkinsprime@gmail.com>
2026-07-14 13:17:46 -0400

    week 1 exercise problems

---
+++ csharp/week-01/testclass1.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+class Week1TestClass
+{
+    static void Main(String[] args)
+    {
+        List<int> scores = new List<int> {850,1200,950,1600};
+
+        foreach(KeyValuePair<string, object> kvp in Mission_Summary(scores))
+        {
+            Console.WriteLine($"{kvp.Key}, {kvp.Value}");
+        }
+
+        scores = new List<int>();;
+
+        foreach(KeyValuePair<string, object> kvp in Mission_Summary(scores))
+        {
+            Console.WriteLine($"{kvp.Key}, {kvp.Value}");
+        }
+        
+    }
+
+    static Dictionary<string, object> Mission_Summary(List<int> damageValues)
+    {
+        int total_damage = 0;
+        int encounters_over_1000 = 0;
+        for (int x = 0; x < damageValues.Count; x ++)
+        {
+            total_damage += damageValues[x];
+            if (damageValues[x] > 1000)
+            {
+                encounters_over_1000++;
+            }
+        }
+        
+        double average_damage = 0;
+        int? highest_damage = 0;
+        if (damageValues.Count <= 0)
+        {
+            average_damage = 0;
+            highest_damage = null;
+        }
+        else
+        {
+            average_damage = Math.Round((double)total_damage / damageValues.Count,2);
+            highest_damage = damageValues.Max();
+        }
+
+        Dictionary<string, object> summary = new ()
+        {
+            {"total_damage",total_damage},
+            {"average_damange",average_damage},
+            {"highest_damange",highest_damage},
+            {"encounters_over_1000",encounters_over_1000}
+        };
+
+        return summary; 
+    }
+}
--- python/week-01/problem_01.py
+++ python/week-01/problem_01.py
@@ -1,5 +1,33 @@
+def mission_summary(damage_values):
+    count = len(damage_values)
+    total_damage = 0
+    encounters_over_1000 = 0
+
+    for damage in damage_values:
+        total_damage += damage
+        if damage > 1000:
+            encounters_over_1000 += 1
+    
+    if count <= 0:
+        average_damage = 0
+        highest_damage = None
+    else:
+        average_damage = total_damage / count
+        highest_damage = max(damage_values)
+
+    return {
+        "total_damage": total_damage,
+        "average_damage": average_damage,
+        "highest_damage": highest_damage,
+        "encounters_over_1000": encounters_over_1000
+    }
+
 def main() -> None:
-    print("Python weekly practice is ready!")
+    scores = [850, 1200, 950, 1600]
+    print(mission_summary(scores))
+
+    scores = []
+    print(mission_summary(scores))
 
 
 if __name__ == "__main__":
